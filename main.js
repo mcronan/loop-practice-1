fe172f593bee3a5c7d8a1248240a7f3c6ae89c6e
Ronan Mc Guire <ronanir600@gmail.com>
2015-06-22 11:56:23 -0600

    loop practice

---
+++ main.js
@@ -0,0 +1,38 @@
+var firstSeq = [0, 100, 200, 300, 400, 500];
+var secondSeq = [1,2,3,4,8, 16, 32, 64];
+var thirdSeq = [1,1,1,2,2,2,3,3,3];
+var fourthSeq = [0,2,4,6,8,10];
+var fifthSeq = [3, 6, 9, 12, 15];
+var sixthSeq = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+var seventhSeq = [0,1,2,3,0,1,2,3,0,1,2,3];
+
+for(var i = 0; i < firstSeq.length; i += 1) {
+	console.log(firstSeq[i]);
+}
+
+for(var i = 0; i < secondSeq.length; i += 1) {
+	console.log(secondSeq[i]);
+}
+
+for(var i = 0; i < thirdSeq.length; i += 1) {
+	console.log(thirdSeq[i]);
+}
+
+for(var i = 0; i < fourthSeq.length; i += 1) {
+	console.log(fourthSeq[i]);
+}
+
+for(var i = 0; i < fifthSeq.length; i += 1) {
+	console.log(fifthSeq[i]);
+}
+
+for(var i = 0; i < sixthSeq.length; i += 1) {
+	console.log(sixthSeq[i]);
+}
+
+for(var i = 0; i < seventhSeq.length; i += 1) {
+	console.log(seventhSeq[i]);
+}
+
+
+
